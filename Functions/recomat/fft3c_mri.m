c5a6dc0bdc940f85a62359e8ad79d71652f35936
Gustav Strijkers <g.j.strijkers@amsterdamumc.nl>
2024-02-22 15:33:49 +0100

    Version 9.7

--- Functions/recomat/fft3c_mri.m
+++
@@ -1,7 +0,0 @@
-function X = fft3c_mri(x)
-
-X=fftshift(ifft(fftshift(x,1),[],1),1)*sqrt(size(x,1));
-X=fftshift(ifft(fftshift(X,2),[],2),2)*sqrt(size(x,2));
-X=fftshift(ifft(fftshift(X,3),[],3),3)*sqrt(size(x,3));
-
-end
